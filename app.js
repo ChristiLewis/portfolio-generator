86e258f3034a38e2ebe742931c3badf08dcccfb1
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-02-16 19:15:54 -0800

    input printed

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Hello Node');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+
+
+const printProfileData = profileDataArr => {
+    //WE LOSE THE () ABOVE AND CHANGE i++ TO i += 1
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }  
+    
+    console.log('================');
+
+    //THE ABOVE IS THE SAME AS THE BELOW
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem) 
+    });
+};
+
+printProfileData(profileDataArgs);
