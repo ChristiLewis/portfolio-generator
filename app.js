8577dd57f0264034feeb0107fdcec87b25c35109
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-02-19 14:03:33 -0800

    specialized updates in the template literal

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 const inquirer = require('inquirer');
 //console.log(inquirer);
 
+///EDIT TO USE FS.WRITEFILE METHOD
+const fs = require('fs');
+///THIS IS A DESTINATION FILE TO RECEIVE THE LOCAL MODULE PAGE-TEMPLATE
+const generatePage = require('./src/page-template.js');
+
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -136,23 +142,17 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', generatePage(portfolioData), err => {
+        if (err) throw new Error(err);
+  
+        console.log('Page created! Check out index.html in this directory to see it!');
+        });
+      
     });
 
 
 
-///EDIT TO USE FS.WRITEFILE METHOD
-//const fs = require('fs');
-///THIS IS A DESTINATION FILE TO RECEIVE THE LOCAL MODULE PAGE-TEMPLATE
-//const generatePage = require('./src/page-template.js');
-///EDIT TO REMOVE THE (2, process.argv.length)- ONLY NEED (2) - REMOVE const profileDataArgs = process.argv.slice(2);
-///ARRAY INDEX EXPRESSIONS with ES6 ASSIGNMENT DESTRUCTURING const [name, github] = profileDataArgs; REPLACED WITH INQUIRER
-//const pageHTML = generatePage(name,github);
 
-//fs.writeFile('./index.html', generatePage(name, github), err => {
-    ///BACK TO if (err) throw err; REMOVE if (err) throw new Error(err);
-    //if (err) throw err;
-
-    //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
 
